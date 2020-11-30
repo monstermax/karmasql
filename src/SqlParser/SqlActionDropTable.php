96e650b3e29bc6a43efcb38dc58243090feb826f
Max F <max@karma-solutions.fr>
2020-11-30 22:00:22 +0100

    add create, drop, truncate commands

---
+++ src/SqlParser/SqlActionDropTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SqlParser;
+
+
+class SqlActionDropTable extends SqlAction
+{
+    protected $table = null;
+
+
+	public function executeAction(SqlExecutor $executor)
+	{
+		$table_drop = $this->table;
+		$table_name = $table_drop->getName();
+
+		$debug = 1;
+
+		$database = $this->parser->getDatabase();
+
+		if (! isset($database[$table_name])) {
+			throw new \Exception("table not exist", 1);
+		}
+
+		unset($database[$table_name]);
+		$table_drop->saveDataToDatabase($this->parser, $database);
+	}
+
+
+
+	public function parseParts()
+	{
+        $drops = iterator_to_array($this->getPart('drop table'));
+
+        if ($drops) {
+			$drops[0]->parsePart();
+			$table = $drops[0]->getTable();
+			
+			$this->table = $table;
+        }
+
+
+        $debug = 1;
+
+    }
+
+}
---
+++ src/SqlParser/SqlActionPartCreateTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SqlParser;
+
+
+class SqlActionPartCreateTable extends SqlActionPart
+{
+	protected $part_name = 'create table';
+    
+    protected $table;
+
+
+	public function parsePart()
+	{
+        $tmp_params = $this->getParamsFromItems(false);
+        
+        $table_name = $tmp_params[0]->word;
+
+        $this->table = new SqlTable($table_name);
+
+        $debug = 1;
+
+        // TODO: detecter keyword "ignore"
+        
+	}
+	
+	
+
+
+    /**
+     * Get the value of table
+     */ 
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    /**
+     * Set the value of table
+     *
+     * @return  self
+     */ 
+    public function setTable($table)
+    {
+        $this->table = $table;
+
+        return $this;
+    }
+
+}
